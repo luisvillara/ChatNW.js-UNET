3c33cf5acd8756624c21b26194b4cb6fbc5cd70f
José Roa <jg.roac@gmail.com>
2015-07-01 20:47:50 -0430

    bullshit

--- app/scripts/createOrJoin.js
+++ app/scripts/createOrJoin.js
@@ -1,5 +1,6 @@
-var myIP = require('my-ip');
-var _ = require('underscore');
+var myIP = require('my-ip'),
+    MD5 = require('MD5'),
+    _ = require('underscore');
 
 var ip = myIP('IPv6');
 var multicast = network.multicast(5000);
@@ -10,19 +11,37 @@ var templates = {
 
 multicast.socket.on('message',function(message,rinfo){
   console.log(message.toString());
-  handledRoom(JSON.parse(message));
+  var json = JSON.parse(message);
+
+
+
+  if( json.COD === 10 )
+    handleRoom(json.SALA);
+
 });
 
-function handledRoom(room){
-  console.log(room);
-  if(!_.contains(rooms,room.ip)){
-    rooms.push(room.ip);
-    $('#rooms').append(templates.room(room));
+function handleRoom(room){
+
+  if(!_.findWhere(rooms,{ id: room.id })){
+    rooms.push(room);
+    renderRoom(room);
   }
 
 }
 
+function renderRoom(room){
+  $('#rooms').append(templates.room(room));
+}
 
+
+//Events
+
+$('#rooms').on('click','.presentation a',function(e){
+  e.preventDefault();
+  var room = _.findWhere(rooms,{id : e.currentTarget.id});
+  dialog(room);
+  console.log(room);
+});
 
 $('#add-room').on('click',function(e){
   e.preventDefault();
@@ -55,15 +74,59 @@ $('#add-room').on('click',function(e){
               };
 
               global.infoChat.room = {
+                id: MD5(ip),
                 name: roomname,
                 ip: ip
               };
 
               multicast.socket.close();
-              window.location.href = 'groupchat.html';
+              window.location.href = 'server.html';
             }
           }
         }
       }
   });
 });
+
+function dialog(data){
+  bootbox.dialog({
+      title:"Nombre de Usuario",
+      message: '<div class="row">'+
+          '<div class="col-md-12">'+
+          '<div class="form-group">'+
+          '<input id="name" type="text" placeholder="Tu Nombre" class="form-control input-md">'+
+          '</div>'+
+          '</div>'+
+          '</div>',
+      buttons: {
+        success : {
+          label: "<strong>Continuar</strong>",
+          className: "btn-success",
+          callback: function(){
+
+            var name = $('#name').val();
+
+            if(name === ''){
+              console.log('Existen campos vacios');
+            }else{
+
+              global.infoChat.user = {
+                name : name,
+                ip: ip
+              };
+
+              global.infoChat.room = {
+                id: MD5(data.ip),
+                name: data.name,
+                ip: data.ip,
+                users: data.users
+              };
+
+              multicast.socket.close();
+              window.location.href = 'client.html';
+            }
+          }
+        }
+      }
+  });
+}
--- app/scripts/network.js
+++ app/scripts/network.js
@@ -1,25 +1,30 @@
 var network = {
   multicast : function(port){
-    var dgram = require('dgram');
+    var myIP = require('my-ip');
+    var dgram = require('dgram')
     var multicastAddress = 'ff02::1234';
     var socket = dgram.createSocket('udp6');
+    var ip = myIP('IPv6');
+
     this.socket = socket;
     this.address = multicastAddress;
     this.port = port;
 
 
     socket.bind(port,function(){
+
       socket.setMulticastTTL(128);
-      socket.addMembership(multicastAddress);
+      socket.addMembership(multicastAddress,ip);
       console.log('listening on :' + multicastAddress);
     });
 
-    this.send = function(message){
+    this.send = function(message,destinationPort){
+
       var data = new Buffer(message);
 
-      socket.send(data,0,data.length,5000,multicastAddress,function(err){
+      socket.send(data,0,data.length,destinationPort,multicastAddress,function(err){
         if(err) throw err;
-        console.log('message sended:' + message);
+        //console.log('message sended:' + message);
       });
 
     };
